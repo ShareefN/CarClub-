1c0db69196a4b0377fbbc9f34ba76a0ddc0a9373
Queenrashh <queenrashh@queenrashhs-macbook-pro.home>
2019-07-14 18:22:44 +0300

    backend completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
 function App() {
-  constructor(props){
-    super(props)
-    this.state = {
-
-    }
-  }
-
   render(){
     return(
       <div>
-        <h1></h1>
+        
       </div>
     )
   }
